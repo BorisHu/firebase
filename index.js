31fdee761d5c8a86d0d58d2744fcd436cb334f2a
BorisHu <91997134+BorisHu@users.noreply.github.com>
2023-03-02 15:02:41 -0800

    add login

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {
+  getAuth,
+  signInAnonymously,
+  onAuthStateChanged,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+
+import {
     getFirestore,
     collection,
     addDoc,
@@ -31,13 +39,54 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 let messages = [];
 const messagesRef = collection(db, "score");
+const auth = getAuth();
+let username = "anon";
+
+function signInAnon() {
+  signInAnonymously(auth)
+    .then(() => {
+      updateProfile(auth.currentUser, { displayName: username })
+      console.log(auth.currentUser);
+    })
+    .catch((error) => {
+      console.error(`Error ${error.code}: ${error.message}.`);
+    });
+}
+
+// This function is called if the Sign Out button is clicked
+function signOutUser() {
+  signOut(auth)
+    .then(() => {
+      console.info("Sign out was successful");
+    })
+    .catch((error) => {
+      console.error(`Error ${error.code}: ${error.message}.`);
+    });
+}
+
+// This is an observer which runs whenever the authentication state is changed
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    console.log("AUTH STATE CHANGED");
+    const uid = user.uid;
+
+    // If there is an authenticated user, we render the normal view
+    render(view(), document.body);
+    // getAllMessages();
+  } else {
+    // Otherwise, we render the sign in view
+    render(signInView(), document.body);
+  }
+});
 
 async function sendMessage(message) {
 console.log("Sending a score!");
+const user = auth.currentUser;
 // Add some data to the messages collection
 try {
     const docRef = await addDoc(collection(db, "score"), {
-    time: Date.now(),
+    displayName: user.isAnonymous ? "anon" : user.displayName,
+    uid: user.uid,
     content: Number(message),
     });
     console.log("Document written with ID: ", docRef.id);
@@ -46,47 +95,62 @@ try {
 }
 }
 
-  async function getAllMessages() {
-    messages = [];
-  
-    const querySnapshot = await getDocs(
-      query(messagesRef, orderBy("content", "desc"))
-    );
-    querySnapshot.forEach((doc) => {
-      let msgData = doc.data();
-      messages.push(msgData);
-    });
-  
-    console.log(messages);
-    render(view(), document.body);
-  }
-  
-  getAllMessages();
+async function getAllMessages() {
+  messages = [];
 
-  function handleInput(e) {
-    if (e.key == "Enter") {
-      sendMessage(e.target.value);
-      e.target.value = "";
-    }
-  }
-
-  function view() {
-    return html`<h1>my cool app</h1>
-      <input type="text" @keydown=${handleInput} />
-      <div id="messages-container">
-        ${messages.map((msg) => html`<div class="message">${msg.content}</div>`)}
-      </div>`;
-  }
-
-  onSnapshot(
-    collection(db, "score"),
-    (snapshot) => {
-      console.log("snap", snapshot);
-      getAllMessages();
-    },
-    (error) => {
-      console.error(error);
-    }
+  const querySnapshot = await getDocs(
+    query(messagesRef, orderBy("content", "desc"))
   );
+  querySnapshot.forEach((doc) => {
+    let msgData = doc.data();
+    messages.push(msgData);
+  });
 
-render(view(), document.body);
+  console.log(messages);
+  render(view(), document.body);
+}
+
+
+function handleInput(e) {
+  if (e.key == "Enter") {
+    sendMessage(e.target.value);
+    e.target.value = "";
+  }
+}
+
+function signInView() {
+  return html`
+  <h3>Enter User name:</h3>
+  <input type="text" @keydown=${handleInput} />
+  <button class="sign-in" @click=${signInAnon}>Sign in</button>
+  <h3>Or:</h3>
+  <button class="sign-in" @click=${signInAnon}>Anonymous Sign in</button>`;
+}
+
+function view() {
+  let user = auth.currentUser;
+  return html`
+  <h1>Game</h1>
+  <div id="top-bar">
+  <span
+    >Signed in as
+    ${user.isAnonymous ? "anon" : auth.currentUser.displayName}</span
+  >
+  <button @click=${signOutUser}>Sign Out</button>
+</div>
+    <input type="text" @keydown=${handleInput} />
+    <div id="messages-container">
+      ${messages.map((msg) => html`<div class="message">${msg.displayName,msg.content}</div>`)}
+    </div>`;
+}
+
+onSnapshot(
+  collection(db, "score"),
+  (snapshot) => {
+    console.log("snap", snapshot);
+    getAllMessages();
+  },
+  (error) => {
+    console.error(error);
+  }
+);
